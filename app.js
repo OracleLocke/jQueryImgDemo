61969f5bbe246df0b74447ffe29f6dfee03360d0
Joshua Beckemeyer <joshuabeckemeyer@gmail.com>
2017-10-07 12:52:30 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// var img1 = {
+// 	content:url('source.unsplash.com/rTZW4f02zY8')
+// }
+
+$("#img1").on('click', function(){
+	$("img").attr('src', 'nasa-63032.jpg');
+});
+
+$("#img2").on('click', function(){
+	$("img").attr('src', 'nasa-89125.jpg');
+});
+
+$("#img3").on('click', function(){
+	$("img").attr('src', 'spacex-71873.jpg');
+});
+
+$("#original").on('click', function(){
+	$("img").attr('src', 'nasa-63029.jpg');
+});
